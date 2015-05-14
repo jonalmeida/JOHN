02528a6b10a23dd6d38962a9fdd6b916333642c3
Jonathan Almeida <git@jonalmeida.com>
2015-05-14 02:24:58 -0400

    Part 3 - News Item layout alignment (minor)

--- app/src/main/java/com/jonalmeida/john/NewsItemsRecyclerActivity.java
+++ app/src/main/java/com/jonalmeida/john/NewsItemsRecyclerActivity.java
@@ -3,6 +3,7 @@ package com.jonalmeida.john;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.util.Log;
 
 import com.firebase.client.Firebase;
 
