607c0f78ba9f87837262eac121a51024b5df5b80
Jonathan Almeida <git@jonalmeida.com>
2015-05-15 12:41:53 -0400

    Adding Firebase top stories ID to recycler

--- app/src/main/java/com/jonalmeida/john/NewsItemsRecyclerFragment.java
+++ app/src/main/java/com/jonalmeida/john/NewsItemsRecyclerFragment.java
@@ -6,9 +6,16 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.firebase.client.ChildEventListener;
+import com.firebase.client.DataSnapshot;
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+import com.firebase.client.ValueEventListener;
 
 import java.util.LinkedList;
 
@@ -22,26 +29,28 @@ import java.util.LinkedList;
 public class NewsItemsRecyclerFragment extends Fragment {
 
     private RecyclerView mRecyclerView;
-    private RecyclerView.Adapter mAdapter;
+    private NewsItemsRecyclerViewAdapter mAdapter;
     private LinearLayoutManager mLinearLayoutManager;
 
     private LinkedList<NewsItem> items;
-
+    private Firebase fb;
 
     /**
      * Mandatory empty constructor for the fragment manager to instantiate the
      * fragment (e.g. upon screen orientation changes).
      */
     public NewsItemsRecyclerFragment() {
-
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Firebase.setAndroidContext(getActivity().getApplicationContext());
+        fb = new Firebase("https://hacker-news.firebaseio.com/v0");
         items = new LinkedList<>();
         items.push(new NewsItem("Foo bar story title", "http://example.com", 9, "jonalmeida", 0));
         items.push(new NewsItem("Foo bar story title", "http://example.com", 9, "jonalmeida", 0));
+        populateList();
     }
 
     @Override
@@ -56,6 +65,7 @@ public class NewsItemsRecyclerFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
         mAdapter = new NewsItemsRecyclerViewAdapter(items);
         mRecyclerView.setAdapter(mAdapter);
+        mAdapter.addItem(new NewsItem("Foo bar story title", "http://notexample.com", 9, "jonalmeida", 0));
     }
 
     @Override
@@ -79,6 +89,29 @@ public class NewsItemsRecyclerFragment extends Fragment {
         mRecyclerView = (RecyclerView) v.findViewById(R.id.recycler_view);
         mRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
         // Do Firebase setting here
+    }
+
+    private void populateList() {
+        fb.child("topstories").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                Log.d("RecycleFrag", "Full spitting: " + dataSnapshot.toString());
+                for (DataSnapshot ds : dataSnapshot.getChildren()) {
+//                    Log.d("RecycleFrag", ds.toString());
+                    Log.d("RecyclerFlag", "Value trying to add: " + ds.getValue().toString());
+                    insertNewsItem(ds);
+                }
+            }
+
+            @Override
+            public void onCancelled(FirebaseError firebaseError) {
+
+            }
+        });
+    }
+
+    private void insertNewsItem(DataSnapshot ds) {
+        items.push(new NewsItem(ds.getValue().toString(), "http://example.com", 0, "jonalmeida", 1));
     }
 
 }
