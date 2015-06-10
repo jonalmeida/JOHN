4cfe7df6eb838071b7213e0e752d2e264df98483
Jonathan Almeida <git@jonalmeida.com>
2015-06-09 22:18:39 -0400

    Launch Settings intent for details activity

--- app/src/main/java/com/jonalmeida/john/NewsItemDetailActivity.java
+++ app/src/main/java/com/jonalmeida/john/NewsItemDetailActivity.java
@@ -54,19 +54,17 @@ public class NewsItemDetailActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-        if (id == android.R.id.home) {
-            // This ID represents the Home or Up button. In the case of this
-            // activity, the Up button is shown. Use NavUtils to allow users
-            // to navigate up one level in the application structure. For
-            // more details, see the Navigation pattern on Android Design:
-            //
-            // http://developer.android.com/design/patterns/navigation.html#up-vs-back
-            //
-            NavUtils.navigateUpTo(this, new Intent(this, NewsItemsRecyclerActivity.class));
-            return true;
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                NavUtils.navigateUpTo(this, new Intent(this, NewsItemsRecyclerActivity.class));
+                return true;
+            case R.id.action_settings:
+                Intent intent = new Intent(this, MainSettingsActivity.class);
+                startActivity(intent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
         }
-        return super.onOptionsItemSelected(item);
     }
 
     @Override
